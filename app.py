1711d68bef56e143e42d115191f949f29c69656f
abylaikhan28-sh <230103105@sdu.edu.kz>
2025-10-20 01:11:00 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 from flask import Flask, jsonify
 
+
 app = Flask(__name__)
+
 
 @app.route('/')
 def home():
     return jsonify({"message": "Hello DevOps API"})
 
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5001)
+    app.run(debug=True, host='0.0.0.0', port=5000)
